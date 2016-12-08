6dbe6f37286b93e3292123f86840a05f38fa4568
EthanBerroa <ethan.berroa@gmail.com>
2016-12-08 16:12:55 -0500

    added tag and photo from previous java project

---
+++ app/src/main/java/com/example/dell/photoapp53/Photo.java
@@ -0,0 +1,102 @@
+package com.example.dell.photoapp53;
+
+/**
+ * Created by Dell on 12/8/2016.
+ */
+
+import android.net.Uri;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class Photo implements java.io.Serializable{
+
+    //private BufferedImage img;
+    //private Calendar calendar;
+    private String URI;
+    private static final long serialVersionUID = 2L;
+
+    public ArrayList<Tag> tags;
+
+    public Photo(Uri uri){
+        this.URI = uri.toString();
+        this.tags = new ArrayList<Tag>();
+    }
+
+
+    public String getURI() {
+        // TODO Auto-generated method stub
+        return this.URI;
+    }
+
+
+    public boolean equals (Object o)
+    {
+        if (!(o instanceof Photo)) return false;
+        else{
+            Photo photo = (Photo)o;
+            if (photo.getURI().equals(this.getURI())){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+
+    public boolean containsTag(Tag tmp)
+    {
+        for(int i = 0; i < tags.size(); i++)
+        {
+            if(tmp.equals(tags.get(i)))
+                return true;
+        }
+        return false;
+    }
+
+
+    public boolean addTag(Tag tmp)
+    {
+        if(!this.containsTag(tmp))
+        {
+            tags.add(tmp);
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    private void writeObject(java.io.ObjectOutputStream stream)
+            throws IOException
+    {
+        stream.writeObject(URI);
+
+
+        stream.writeObject(tags);
+
+    }
+
+
+    public void setURI(String URI){
+        this.URI = URI;
+    }
+
+
+    private void readObject(java.io.ObjectInputStream stream)
+            throws IOException, ClassNotFoundException
+    {
+        URI = (String) stream.readObject();
+
+
+        tags = (ArrayList<Tag>)stream.readObject();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dell/photoapp53/Tag.java
@@ -0,0 +1,91 @@
+package com.example.dell.photoapp53;
+
+/**
+ * Created by Dell on 12/8/2016.
+ */
+
+import java.io.IOException;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Tag object class. A tag is simply a "type" string (either "Location", "Name", or "Other") and a value string.
+ * @author Ethan Berroa
+ * @author Milan Patel
+ *
+ */
+
+public class Tag implements java.io.Serializable
+{
+    private String type;
+    private String value;
+    //public ArrayList<Photo> photos;
+
+    /**
+     * Empty constructor.
+     */
+    public Tag(){
+
+    }
+
+    /**
+     * Tag constructor that takes in type and value parameters.
+     * @param type
+     * @param value
+     */
+    public Tag(String type, String value){
+        this.type = type;
+        this.value = value;
+    }
+
+
+
+    public String getType(){
+        return type;
+    }
+
+
+    public String getValue(){
+        return value;
+    }
+
+
+    private void writeObject(java.io.ObjectOutputStream stream)
+            throws IOException
+    {
+        stream.writeObject(type);
+        stream.writeObject(value);
+    }
+
+
+    private void readObject(java.io.ObjectInputStream stream)
+            throws IOException, ClassNotFoundException
+    {
+        type = (String) stream.readObject();
+        value = (String)stream.readObject();
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+        if (!(o instanceof Tag)) return false;
+        else{
+            Tag tag = (Tag)o;
+            if (tag.getType().equals(this.getType()) && (tag.getValue().equals((this.getValue())))){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    public String toString(){
+        return type + ":  " + value;
+    }
+
+}
